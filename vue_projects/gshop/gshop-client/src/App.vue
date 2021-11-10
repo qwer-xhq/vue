258f97790cb72481f65cf144a26533e3cdb39951
qwer-xhq <1533068859@qq.com>
2021-11-10 17:39:12 +0800

    commit

---
+++ vue_projects/gshop/gshop-client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <mainTabBar></mainTabBar>
+  </div>
+</template>
+
+<script>
+  import mainTabBar from 'components/content/mainTabbar/MainTabBar'
+  export default {
+    name: "App",
+    components: {
+      mainTabBar,
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
---
+++ vue_projects/gshop/gshop-client/src/views/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home">
+    <NavBar class="home-nav"><template v-slot:center>购物街</template></NavBar>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+    },
+    data(){
+      return {
+        banners:[],
+        recommend:[],
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.banner.list
+        this.recommend = res.data.recommend.list
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
