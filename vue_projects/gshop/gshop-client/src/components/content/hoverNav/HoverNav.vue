c33b0576d8891feba099bb09b6933a05c80dc2f3
qwer-xhq <1533068859@qq.com>
2021-11-13 23:27:37 +0800

    commit

---
+++ vue_projects/gshop/gshop-client/src/components/content/hoverNav/HoverNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hover-nav">
+    <div v-for="(item,index) in titles" @click="active(index)">
+      <span :class="{active: currentIndex===index}">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HoverNav',
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0,
+      }
+    },
+    methods: {
+      active(index){
+        this.currentIndex=index
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .hover-nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #eee;
+    height: 40px;
+  }
+  .hover-nav span {
+    padding: 3px;
+  }
+  .active {
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint);
+    
+  }
+</style>
--- vue_projects/gshop/gshop-client/src/views/home/Home.vue
+++ vue_projects/gshop/gshop-client/src/views/home/Home.vue
@@ -3,14 +3,62 @@
     <NavBar class="home-nav"><template v-slot:center>购物街</template></NavBar>
     <HomeSwiper :banners="banners"></HomeSwiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <FeatureView></FeatureView>
+    <HoverNav class="hover-nav" :titles="['流行','新款','精选']"></HoverNav>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
-  import {getHomeMultidata} from "network/home";
+  import HoverNav from 'components/content/hoverNav/HoverNav'
+
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView"
+
+  import {getHomeMultidata} from "network/home";
 
   export default {
     name: "Home",
@@ -18,6 +66,8 @@
       NavBar,
       HomeSwiper,
       RecommendView,
+      FeatureView,
+      HoverNav
     },
     data(){
       return {
@@ -36,8 +86,16 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+  }
+  .hover-nav{
+    position: sticky;
+    top: 44px
   }
 </style>
